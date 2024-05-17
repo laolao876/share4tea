c012fa58091b03a3881254ca6543c7ede5ffb6c9
laolao876 <li.xiaolong@126.com>
2024-05-17 15:34:29 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+// index.js 入口文件
+import Format from "./src/format";
+import Validate from "./src/validate";
+
+export { Format, Validate };
---
+++ node_modules/npm-test_mrlee/test.js
@@ -0,0 +1,5 @@
+const Validate = require('./src/validate')
+const Format = require('./src/format')
+ 
+console.log(Validate.mobileCheck('13033333333'))
+console.log(Format.formatMoney(123456789))
